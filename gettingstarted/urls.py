0cf5a70ee884b40632b00573cda05505e19bd3bf
Sean Auriti <sean@alphaonelabs.com>
2016-10-04 22:16:10 -0400

    Launch web application closes #3 and integrate user signup closes #4

---
+++ gettingstarted/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import include, url
+from website.views import UserProfileDetailView
+from django.contrib import admin
+admin.autodiscover()
+
+import website.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', website.views.index, name='index'),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^db', website.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^profile/$', website.views.profile, name='profile'),
+    url(r'^profile/(?P<slug>[^/]+)/$', UserProfileDetailView.as_view(), name="profile"),
+]
---
+++ website/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.http import Http404
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+
+    return render(request, 'index.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
+
+def profile(request):
+    try:
+        return redirect('/profile/' + request.user.username)
+    except Exception:
+        return redirect('/')
+
+
+class UserProfileDetailView(DetailView):
+    model = get_user_model()
+    slug_field = "username"
+    template_name = "profile.html"
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            messages.error(self.request, 'That user was not found.')
+            return redirect("/")
+        return super(UserProfileDetailView, self).get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        user = super(UserProfileDetailView, self).get_object(queryset)
+        return user
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileDetailView, self).get_context_data(**kwargs)
+        return context
